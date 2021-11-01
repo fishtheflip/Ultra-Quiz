01cbc86d4b60e31ce4f4063ba7f8c28d8b666a32
Andrei Parkhomenko <ataflava@gmail.com>
2021-11-01 19:18:00 +0600

    some little chaneges

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,25 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = 5000
+
+const dataJS = require('./data/data-js')
+const dataHtml = require('./data/data-html')
+const dataCss = require('./data/data-css')
+
+app.use(cors())
 
 app.get('/', (req, res)=>{
     res.send('hello')
+})
+app.get('/js', (req, res)=>{
+    res.send(dataJS)
+})
+app.get('/html', (req, res)=>{
+    res.send(dataHtml)
+})
+app.get('/css', (req, res)=>{
+    res.send(dataCss)
 })
 
 app.listen(port, ()=>{
